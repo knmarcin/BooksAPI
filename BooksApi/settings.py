d1f630313858cf1f97ed09de1b7f540b51fca3dc
knmarcin <Imspy4pcp@>
2021-08-10 17:39:29 +0200

    db switch

--- BooksApi/settings.py
+++ BooksApi/settings.py
@@ -69,14 +69,7 @@ WSGI_APPLICATION = 'BooksApi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # db_config is stored in git excluded config.py, for security reasons
-# DATABASES = {'default':dj_database_url.config()}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
-}
+DATABASES = {'default':dj_database_url.config()}
 
 
 # Password validation
