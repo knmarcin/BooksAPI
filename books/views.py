4e1563d4c9084ac95640c9ad6a9649f2fbe7920f
knmarcin <Imspy4pcp@>
2021-08-10 13:05:28 +0200

    possible bug fix

--- books/views.py
+++ books/views.py
@@ -24,6 +24,7 @@ class BooksDetailSet(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book
     serializer_class = BookFullSerializer
 
+
 @api_view(['POST'])
 def download_books_from_api(request, *args, **kwargs):
     serializer = GetBookSerializer(data=request.data)
