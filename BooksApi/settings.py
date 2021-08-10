eef23a7b275937e7b46530fe2dcb46c7c1f063f2
knmarcin <Imspy4pcp@>
2021-08-10 11:03:24 +0200

    deploying to heroku

--- BooksApi/settings.py
+++ BooksApi/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from . import config
 import django_heroku
@@ -110,6 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
