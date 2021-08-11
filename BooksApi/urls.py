f14f228f971955d8a8b691d5fd4de65114ad550e
knmarcin <Imspy4pcp@>
2021-08-11 08:31:30 +0200

    hide admin page

--- BooksApi/urls.py
+++ BooksApi/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from books.views import BooksViewSet, BooksDetailSet, download_books_from_api
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('books/', BooksViewSet.as_view()),
     path('books/<int:pk>', BooksDetailSet.as_view()),
     path('db/', download_books_from_api),
